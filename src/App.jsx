b236e02623b80a543ac20abd5df8365ccaec3128
James <jenningsje@mac.com>
2026-07-30 12:32:45 -0500

    delete frontend this is backend

--- src/App.jsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:40b4757ab1825ac77074c3b720897528ff38a40a008f5eda40dd7b8915dfeb00
-size 3680
--- vite.config.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c5c6874b3cb6a334a6df76459c6ac9c012ed6ce59dd9255ddb03ec115d7b5b86
-size 359
