15727818d858128c31739b6e289d0adfe342a778
James <jenningsje@mac.com>
2026-07-30 12:47:45 -0500

    the command line search engine works just need to modify it to take in a json now

--- src/setup.sh
+++ src/setup.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bba5fb7c71ebc14052ae62fc4193f25cc7710f67ea2a2d9e76e5f25dc2ae095f
-size 462
+oid sha256:6f027b5e9489223260f470ad232b1f2e43ecf032ee65de52ab1d338939525fbd
+size 263
